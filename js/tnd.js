c3d33842a27dc29e52111c39b9a02ee539ebf12c
isora <ijimerusora69@gmail.com>
2021-09-20 15:44:42 +0800

    9/20/21

---
+++ js/tnd.js
@@ -0,0 +1,4 @@
+var dt = new Date();
+document.getElementById("tnd").innerHTML = 
+(("0"+dt.getDate()).slice(-2)) +"/"+ (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (dt.getFullYear()) +" "
++ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2)) +":"+ (("0"+dt.getSeconds()).slice(-2));
